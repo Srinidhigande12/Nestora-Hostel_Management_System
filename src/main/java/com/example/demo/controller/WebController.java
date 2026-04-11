45a8cea35774f5757144110942a0d1f32c1357d4
Srinidhigande12 <2410030204@klh.edu.in>
2026-04-11 21:56:17 +0530

    fix duplicate mapping final

--- src/main/java/com/example/demo/controller/WebController.java
+++ src/main/java/com/example/demo/controller/WebController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WebController {
 
+    @GetMapping("/")
+    public String home() {
+        return "forward:/index.html";
+    }
+
     @GetMapping(value = "/{path:[^\\.]*}")
     public String redirect() {
         return "forward:/index.html";
